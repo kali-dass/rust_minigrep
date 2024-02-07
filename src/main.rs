590388434bcca55fcdb063d593c6167de3fa3739
Kalidass Mookkaiah <kalidass@gmail.com>
2024-02-07 16:49:35 +0000

    split out config and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
 
-    let (query,file_path)=parse_config(&args);
+    let config=Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!(" in File {}", file_path);
+    println!("Searching for {}", config.query);
+    println!(" in File {}", config.file_path);
 
-    let content = fs::read_to_string(file_path)
-                    .expect("Should have been able to read the file");
-    
-    println!("With text:\n{content}");
+    run(config);
 }
 
-fn parse_config(args: &[String] ) -> (&str, &str)
-{
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config{
+    query: String,
+    file_path: String,
+}
 
-    (query,file_path)
+impl Config{
+    fn build(args: &[String] ) -> Result<Config,&'static str>{
+
+        if args.len() < 3 {
+            return Err("not enough arguments passed");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config{query,file_path})
+    }
+}
+
+fn run(config: Config) -> Result<(),Box<dyn Error>>{
+    let content = fs::read_to_string(config.file_path)?;
+    
+    println!("With text:\n{content}");
+
+    Ok(())
 }
